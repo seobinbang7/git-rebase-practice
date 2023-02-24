518add910051da86e3625438e34229a39092b57e
방서빈 <newmso5382@gmail.com>
2023-02-24 13:08:12 +0900

    Add: login 페이지 기능 구현

--- Frontend/git-rebase-test/src/pages/Login/Login.js
+++ Frontend/git-rebase-test/src/pages/Login/Login.js
@@ -47,30 +47,3 @@ export default function Login() {
     </div>
   );
 }
-import React, { useState } from "react";
-import "./Login.css";
-
-export default function Login() {
-  return (
-    <div className="login">
-      <main className="loginContainer">
-        <h1 className="title">Git Rebase Test</h1>
-        <form className="loginForm">
-          <input
-            className="userId"
-            type="text"
-            placeholder="이메일을 입력해 주세요."
-          />
-          <input
-            className="userPassword"
-            type="password"
-            placeholder="비밀번호를 입력해 주세요."
-          />
-          <button className="loginButton" type="button">
-            로그인
-          </button>
-        </form>
-      </main>
-    </div>
-  );
-}
